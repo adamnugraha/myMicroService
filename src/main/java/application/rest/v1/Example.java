0696cc9859688194e57f7e41209e60a3740b98c0
Winton <wintonjkt@gmail.com>
2018-04-19 11:37:08 -0700

    edit code

--- src/main/java/application/rest/v1/Example.java
+++ src/main/java/application/rest/v1/Example.java
@@ -19,7 +19,7 @@ public class Example {
     public Response example() {
         List<String> list = new ArrayList<>();
         //return a simple list of strings
-        list.add("{\"Demo Event IBM Jakarta\":\"I am a bad bad bad developer\"}");
+        list.add("{\"Demo Event IBM Jakarta\":\"I am a not bad bad bad developer\"}");
         return Response.ok(list.toString()).build();
     }
 
